bbb061fef7c7525c97dfa5ee1024a5d02178c526
ShinteiMai <stevenhanselgo@gmail.com>
2020-10-11 10:30:31 +0700

    :rocket: Finished vanilla build

--- src/components/TodosInput.tsx
+++ src/components/TodosInput.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Box, Button, Input } from "@chakra-ui/core";
+import { Todo } from "../types";
 
-const TodosInput: React.FC = () => {
+interface Props {
+  todos: Todo[];
+  addTodo: (todo: Todo) => void;
+}
+
+const TodosInput: React.FC<Props> = (props) => {
+  const [value, setValue] = useState<string>("");
   return (
     <Box display="flex">
-      <Input placeholder="Write a task..." />
-      <Button>Add</Button>
+      <Input
+        placeholder="Write a task..."
+        value={value}
+        onChange={(e: React.FormEvent<HTMLInputElement>) =>
+          setValue(e.currentTarget.value)
+        }
+      />
+      <Button
+        onClick={() => {
+          props.addTodo({
+            id: `${props.todos.length - 1}-${value}`,
+            task: value,
+          });
+          setValue("");
+        }}
+      >
+        Add
+      </Button>
     </Box>
   );
 };
